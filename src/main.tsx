acbbf985aab178211bf8dc48916c4fc717972c28
Silicon 01 <bot+01@kyya.dev>
2026-02-09 10:37:58 +0000

    chore: remove wechat-copy plugin from public repo

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./themes";
 import App from "./App";
-import { pluginRegistry } from "./plugins/registry";
-import { wechatCopyPlugin } from "./plugins/wechat-copy";
-
-pluginRegistry.register(wechatCopyPlugin);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
