04081ff155ee99890cfe12619be7d307957bcff3
Silicon 01 <bot+01@kyya.dev>
2026-02-09 10:46:22 +0000

    feat: expose React global for external plugin support

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./themes";
 import App from "./App";
 import { pluginRegistry } from "./plugins/registry";
+
+// Expose React for external plugins
+(window as any).__MV_REACT__ = React;
 
 // Load external plugins (non-blocking)
 pluginRegistry.loadExternal();
